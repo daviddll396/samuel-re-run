090866089489679f1a9fcd260e7ebdafd9ef9644
David Fagbenja <daviddll396@gmail.com>
2025-02-13 18:31:21 +0100

    Home changess

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,6 +1,6 @@
 import { useMovieContext } from "../../contexts/MovieContext";
 import MovieCard from "../../components/MovieCard/MovieCard";
-import "./favorites.css";
+import "./Favorites.css";
 
 const Favorites = () => {
   const { favorites } = useMovieContext();
